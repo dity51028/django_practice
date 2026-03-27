226d7bff8f3a00e7bd27497781b8f154e713c8be
dity51028 <dipannitamisti10@gmail.com>
2026-03-27 11:10:26 +0530

    template created

---
+++ newProject/newProject/views.py
@@ -0,0 +1,12 @@
+""" Controller of our whole structure """
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+   return render(request, 'index.html')
+def about(request):
+    return HttpResponse("About Us")
+def contact(request):
+    return HttpResponse("Contact Us")
+def services(request):
+    return HttpResponse("Services")
